a9a395d321433e40d02af0464919f295e15eac63
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-19 20:13:32 +0530

    added mapbox

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
+import { useEffect } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import tw from "tailwind-styled-components"
+import mapboxgl from '!mapbox-gl';
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW51c2hrYS0xMiIsImEiOiJja3c2Z3ppaDIyaWFjMm5udDN4eDJtcXg2In0.w1D57PJteFGbN862BpsASw';
+
 
 export default function Home() {
+  useEffect(() => {
+    console.log('hello')
+
+  },[])
   return (
-    <div className={styles.container}>
-      <div className ={styles.map}>Map</div>
-      <div className={styles.start}>Start</div>     
-    </div>
+   <Wrapper>
+     <Map>Map Test</Map>
+     <ActionItems>Start</ActionItems>
+   </Wrapper>
   )
 }
+
+const Wrapper = tw.div`
+flex flex-col bg-red-300 h-screen
+
+`
+const Map = tw.div`
+bg-red-500 flex-1
+`
+const ActionItems = tw.div`
+flex-1
+
+`
