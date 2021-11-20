c7946b0d51091d82edf97ebb9e66b8032b5cd959
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-20 18:21:13 +0530

    added map and updated Map components

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,18 @@ import { useEffect } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import tw from "tailwind-styled-components"
-import mapboxgl from '!mapbox-gl';
+import Map from './components/Map'
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW51c2hrYS0xMiIsImEiOiJja3c2Z3ppaDIyaWFjMm5udDN4eDJtcXg2In0.w1D57PJteFGbN862BpsASw';
+
 
 
 export default function Home() {
-  useEffect(() => {
-    console.log('hello')
+  
 
-  },[])
   return (
    <Wrapper>
-     <Map>Map Test</Map>
+     <Map />
      <ActionItems>Start</ActionItems>
    </Wrapper>
   )
@@ -25,9 +23,7 @@ const Wrapper = tw.div`
 flex flex-col bg-red-300 h-screen
 
 `
-const Map = tw.div`
-bg-red-500 flex-1
-`
+
 const ActionItems = tw.div`
 flex-1
 
