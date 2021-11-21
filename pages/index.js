6daaa071893d959ba2fccf252aec917ce5abf7fc
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-21 22:33:05 +0530

    added  image and Profile components

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,22 @@ export default function Home() {
   return (
    <Wrapper>
      <Map />
-     <ActionItems>Start</ActionItems>
+     <ActionItems>
+       {/*Header*/}
+       <Header>
+         <UberLogo src='https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg' />
+          <Profile>
+            <Name>Anushka Gupta</Name>
+            <UserImage
+            src="https://lh3.googleusercontent.com/a-/AOh14GiD9MBH2IcIdXX8uw40dBSdCkjbAbabGqJPsXaE=s96-c" />
+          </Profile>
+       </Header>
+
+       
+
+
+
+     </ActionItems>
    </Wrapper>
   )
 }
@@ -27,4 +42,19 @@ flex flex-col bg-red-300 h-screen
 const ActionItems = tw.div`
 flex-1
 
+`
+const Header = tw.div`
+`
+
+const UberLogo = tw.img`
+h-28
+`
+
+const Profile = tw.div`
+`
+
+const Name = tw.div`
+`
+
+const UserImage = tw.img`
 `
