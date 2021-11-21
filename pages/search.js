3c69d04c74b8cb34887ef426ff09ccd8041bedaf
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-21 23:58:16 +0530

    updated home page to Ride page

--- pages/search.js
+++ pages/search.js
@@ -7,6 +7,14 @@ const search = () => {
             <ButtonContainer>
                 <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png"></BackButton>
             </ButtonContainer>
+            <InputContainer> 
+            <FormToIcons>
+                <Circle src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png"></Circle>
+                <Line src='https://img.icons8.com/ios/50/'></Line>
+            </FormToIcons>
+
+
+            </InputContainer>
 
 
         </Wrapper>
@@ -21,8 +29,19 @@ bg-gray-200 h-screen
 `
 
 const ButtonContainer = tw.div`
-bg-white
+bg-white px-4
 `
 
 const BackButton = tw.img`
+h-12
 `
+const FormToIcons = tw.div`
+w-10 flex flex-col
+`
+
+const InputContainer = tw.div`
+bg-white
+`
+const Circle = tw.img`
+`
+const Line = tw.img``
