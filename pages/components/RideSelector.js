d446899adaaeda1f03143c6eeb273b7043abaa7e
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-24 23:55:41 +0530

    updates routs and styling

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -6,9 +6,13 @@ const RideSelector = ({pickupCoordinates, dropoffCoordinates}) => {
     const [rideDuration, setRideDuration] = useState(0)
 
     useEffect(() =>{
-        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${dropoffCoordinates[1]};${dropoffCoordinates[0]},${pickupCoordinates[1]}?access_token=pk.eyJIjoiZHJha29zaSIsImEiOiJja2x1YW9jdWswOHcyMnVvZXQ1aTVqcHBnInO.GOSLu_zwAEU9_q8FIkHeaQ`
-        )
-    }, [])
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${dropoffCoordinates[1]};${dropoffCoordinates[0]},${pickupCoordinates[1]}?access_token=pk.eyJIjoiZHJha29zaSIsImEiOiJja2x1YW9jdWswOHcyMnVvZXQ1aTVqcHBnInO.GOSLu_zwAEU9_q8FIkHeaQ`)
+
+        .then(res => res.json())
+        .then(data => {
+            setRideDuration(data.routes[0].duration / 100)
+        })
+    }, [pickupCoordinates, dropoffCoordinates])
   return (
     <Wrapper>
       <Title>Choose a ride, or swipe up for more</Title>
@@ -20,7 +24,7 @@ const RideSelector = ({pickupCoordinates, dropoffCoordinates}) => {
               <Service>{car.Service}</Service>
               <Time>5 min away</Time>
             </CarDetails>
-            <Price>$24.00</Price>
+            <Price>{'$' + (rideDuration * car.multiplier).toFixed(2)} </Price>
           </Car>
         ))}
       </CarList>
--- pages/confirm.js
+++ pages/confirm.js
@@ -3,6 +3,7 @@ import tw from "tailwind-styled-components";
 import Map from "./components/Map";
 import { useRouter } from "next/router";
 import RideSelector from "./components/RideSelector";
+import link from "next/link";
 
 const Confirm = () => {
   const router = useRouter();
@@ -51,6 +52,11 @@ const Confirm = () => {
 
   return (
     <Wrapper>
+      <ButtonContainer>
+        <link href='/search'>
+          <BackButton src='https://img.icons8.com/ios-filled/50/000000/left.png' />
+          </link>
+      </ButtonContainer>
       <Map
         pickupCoordinates={pickupCoordinates}
         dropoffCoordinates={dropoffCoordinates}
@@ -91,3 +97,12 @@ flex-1 flex flex-col h-1/2
 const Wrapper = tw.div`
 flex h-screen flex-col
 `;
+
+
+const ButtonContainer = tw.div`
+rounded-full absolute top-4 left-4 z-10 bg-white shadow-md cursor-pointer
+`
+
+const BackButton = tw.img`
+h-full object-contain
+`
