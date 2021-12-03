75edf463de6ab54f14097600b1410cf595700173
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-12-03 22:30:01 +0530

    fixed build-error

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import tw from "tailwind-styled-components";
-import { carList } from "./data/carList";
+import  {carList}  from "../../public/data/carList.js";
 
 const RideSelector = ({ pickupCoordinates, dropoffCoordinates }) => {
   const [rideDuration, setRideDuration] = useState(0);
